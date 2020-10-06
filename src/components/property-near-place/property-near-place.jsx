5e01f06d36015beba9153280a4ddb9c3bb84d6c7
Natalia <lukashina_natalia@mil.ru>
2020-10-06 21:15:29 +0300

    started work with router, prepared components for each page

--- src/components/property-near-place/property-near-place.jsx
+++ src/components/property-near-place/property-near-place.jsx
@@ -1,27 +1,24 @@
 import React from "react";
 
-const Article = () => {
+const PropertyNearPlace = () => {
   return (
-    <article className="cities__place-card place-card">
-      <div className="place-card__mark">
-        <span>Premium</span>
-      </div>
-      <div className="cities__image-wrapper place-card__image-wrapper">
+    <article className="near-places__card place-card">
+      <div className="near-places__image-wrapper place-card__image-wrapper">
         <a href="#">
-          <img className="place-card__image" src="img/apartment-01.jpg" width="260" height="200" alt="Place image"/>
+          <img className="place-card__image" src="img/room.jpg" width="260" height="200" alt="Place image" />
         </a>
       </div>
       <div className="place-card__info">
         <div className="place-card__price-wrapper">
           <div className="place-card__price">
-            <b className="place-card__price-value">&euro;120</b>
+            <b className="place-card__price-value">&euro;80</b>
             <span className="place-card__price-text">&#47;&nbsp;night</span>
           </div>
-          <button className="place-card__bookmark-button button" type="button">
+          <button className="place-card__bookmark-button place-card__bookmark-button--active button" type="button">
             <svg className="place-card__bookmark-icon" width="18" height="19">
               <use xlinkHref="#icon-bookmark"></use>
             </svg>
-            <span className="visually-hidden">To bookmarks</span>
+            <span className="visually-hidden">In bookmarks</span>
           </button>
         </div>
         <div className="place-card__rating rating">
@@ -31,12 +28,12 @@ const Article = () => {
           </div>
         </div>
         <h2 className="place-card__name">
-          <a href="#">Beautiful &amp; luxurious apartment at great location</a>
+          <a href="#">Wood and stone place</a>
         </h2>
-        <p className="place-card__type">Apartment</p>
+        <p className="place-card__type">Private room</p>
       </div>
     </article>
   );
 };
 
-export default Article;
+export default PropertyNearPlace;
