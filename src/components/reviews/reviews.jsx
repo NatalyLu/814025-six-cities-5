c859ab31af2f822f93c50f9cab5727e6439cfe39
Natalia <lukashina_natalia@mil.ru>
2020-10-24 19:33:25 +0300

    added new components for reviews section

---
+++ src/components/reviews/reviews.jsx
@@ -0,0 +1,54 @@
+import React, {PureComponent} from "react";
+import PropTypes from "prop-types";
+
+class Reviews extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    const hotelReviews = this.props.hotelReviews;
+    return (
+      <ul className="reviews__list">
+        {hotelReviews.map((review, i) => (
+          <li key={`review-${i}`} className="reviews__item">
+            <div className="reviews__user user">
+              <div className="reviews__avatar-wrapper user__avatar-wrapper">
+                <img className="reviews__avatar user__avatar" src={review.avatar} width="54" height="54" alt="Reviews avatar" />
+              </div>
+              <span className="reviews__user-name">
+                {review.nik}
+              </span>
+            </div>
+            <div className="reviews__info">
+              <div className="reviews__rating rating">
+                <div className="reviews__stars rating__stars">
+                  <span style={{width: `80%`}}></span>
+                  <span className="visually-hidden">Rating</span>
+                </div>
+              </div>
+              <p className="reviews__text">
+                {review.review}
+              </p>
+              <time className="reviews__time" dateTime="2019-04-24">{review.month} {review.year}</time>
+            </div>
+          </li>
+        ))}
+      </ul>
+    );
+  }
+}
+Reviews.propTypes = {
+  hotelReviews: PropTypes.arrayOf(PropTypes.shape({
+    nik: PropTypes.string.isRequired,
+    avatar: PropTypes.string.isRequired,
+    review: PropTypes.string.isRequired,
+    data: PropTypes.shape({
+      month: PropTypes.string.isRequired,
+      year: PropTypes.number.isRequired,
+    }).isRequired,
+    starts: PropTypes.number.isRequired,
+  }))
+};
+
+export default Reviews;
