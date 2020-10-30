0e633a86e9092f9a2aa7cd2391e546e9ca91e748
Natalia <lukashina_natalia@mil.ru>
2020-10-31 01:12:56 +0300

    added the ability to create a comment

--- src/components/property-information/property-information.jsx
+++ src/components/property-information/property-information.jsx
@@ -1,113 +1,109 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import Reviews from "../reviews/reviews";
 import ReviewForm from "../review-form/review-form";
-import PropTypes from "prop-types";
+import {offerPropTypes, reviewsShortPropTypes} from "../../prop-types";
 
-const PropertyInformation = (props) => {
-  const {hotel, hotelReviews} = props;
+class PropertyInformation extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  return (
-    <div className="property__wrapper">
-      <div className="property__mark">
-        <span>{hotel.status}</span>
-      </div>
-      <div className="property__name-wrapper">
-        <h1 className="property__name">
-          {hotel.name}
-        </h1>
-        <button className="property__bookmark-button button" type="button">
-          <svg className="property__bookmark-icon" width="31" height="33">
-            <use xlinkHref="#icon-bookmark"></use>
-          </svg>
-          <span className="visually-hidden">To bookmarks</span>
-        </button>
-      </div>
-      <div className="property__rating rating">
-        <div className="property__stars rating__stars">
-          <span style={{width: `80%`}}></span>
-          <span className="visually-hidden">Rating</span>
-        </div>
-        <span className="property__rating-value rating__value">{hotel.stars}</span>
-      </div>
-      <ul className="property__features">
-        <li className="property__feature property__feature--entire">
-          {hotel.type}
-        </li>
-        <li className="property__feature property__feature--bedrooms">
-          {hotel.rooms} Bedrooms
-        </li>
-        <li className="property__feature property__feature--adults">
-          Max {hotel.maxCountOfPeople} adults
-        </li>
-      </ul>
-      <div className="property__price">
-        <b className="property__price-value">&euro;{hotel.price}</b>
-        <span className="property__price-text">&nbsp;night</span>
-      </div>
-      <div className="property__inside">
-        <h2 className="property__inside-title">What&apos;s inside</h2>
-        <ul className="property__inside-list">
-          {hotel.inside.map((inside, i) =>(
-            <li key={`inside-${i}`} className="property__inside-item">
-              {inside}
-            </li>
-          ))}
-        </ul>
-      </div>
-      <div className="property__host">
-        <h2 className="property__host-title">Meet the host</h2>
-        <div className="property__host-user user">
-          <div className="property__avatar-wrapper property__avatar-wrapper--pro user__avatar-wrapper">
-            <img className="property__avatar user__avatar" src={hotel.host.avatar} width="74" height="74" alt="Host avatar" />
+    const offerReviews = this.props.offerReviews;
+    this.state = {
+      reviews: offerReviews
+    };
+  }
+
+  render() {
+    const offer = this.props.offer;
+
+    return (
+      <div className="property__wrapper">
+        {offer.isPremium &&
+          <div className="property__mark">
+            <span>Premium</span>
           </div>
-          <span className="property__user-name">
-            {hotel.host.name}
-          </span>
+        }
+        <div className="property__name-wrapper">
+          <h1 className="property__name">
+            {offer.title}
+          </h1>
+          <button className="property__bookmark-button button" type="button">
+            <svg className="property__bookmark-icon" width="31" height="33">
+              <use xlinkHref="#icon-bookmark"></use>
+            </svg>
+            <span className="visually-hidden">To bookmarks</span>
+          </button>
         </div>
-        <div className="property__description">
-          <p className="property__text">
-            {hotel.host.description}
-            A quiet cozy and picturesque that hides behind a a river by the unique lightness of Amsterdam. The building is green and from 18th century.
-          </p>
-          <p className="property__text">
-            An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.
-          </p>
+        <div className="property__rating rating">
+          <div className="property__stars rating__stars">
+            <span style={{width: `80%`}}></span>
+            <span className="visually-hidden">Rating</span>
+          </div>
+          <span className="property__rating-value rating__value">{offer.rating}</span>
         </div>
+        <ul className="property__features">
+          <li className="property__feature property__feature--entire">
+            {offer.type}
+          </li>
+          <li className="property__feature property__feature--bedrooms">
+            {offer.bedrooms} Bedrooms
+          </li>
+          <li className="property__feature property__feature--adults">
+            Max {offer.maxAdults} adults
+          </li>
+        </ul>
+        <div className="property__price">
+          <b className="property__price-value">&euro;{offer.price}</b>
+          <span className="property__price-text">&nbsp;night</span>
+        </div>
+        <div className="property__inside">
+          <h2 className="property__inside-title">What&apos;s inside</h2>
+          <ul className="property__inside-list">
+            {offer.inside.map((inside, i) =>(
+              <li key={`inside-${i}`} className="property__inside-item">
+                {inside}
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className="property__host">
+          <h2 className="property__host-title">Meet the host</h2>
+          <div className="property__host-user user">
+            <div className={`property__avatar-wrapper ${offer.host.isPro && `property__avatar-wrapper--pro`} user__avatar-wrapper`}>
+              <img className="property__avatar user__avatar" src={offer.host.avatar} width="74" height="74" alt="Host avatar" />
+            </div>
+            <span className="property__user-name">
+              {offer.host.name}
+            </span>
+          </div>
+          <div className="property__description">
+            <p className="property__text">
+              {offer.description}
+            </p>
+            <p className="property__text">
+              An independent House, strategically located between Rembrand Square and National Opera, but where the bustle of the city comes to rest in this alley flowery and colorful.
+            </p>
+          </div>
+        </div>
+        <section className="property__reviews reviews">
+          <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
+
+          <Reviews offerReviews={this.state.reviews} />
+          {/* onNewReview={onNewReview}/> */}
+
+          {/* <ReviewForm /> */}
+          <ReviewForm onNewReview={(review) => {
+            this.setState({reviews: [...this.state.reviews, review]});
+          }} />
+        </section>
       </div>
-      <section className="property__reviews reviews">
-        <h2 className="reviews__title">Reviews &middot; <span className="reviews__amount">1</span></h2>
-
-        <Reviews hotelReviews={hotelReviews} />
-        {/* onNewReview={onNewReview}/> */}
-
-        <ReviewForm />
-      </section>
-    </div>
-  );
-};
+    );
+  }
+}
 
 PropertyInformation.propTypes = {
-  hotelReviews: PropTypes.array,
-  // onNewReview: PropTypes.func.isRequired,
-
-  hotel: PropTypes.shape({
-    name: PropTypes.string.isRequired,
-    address: PropTypes.string.isRequired,
-    status: PropTypes.string.isRequired,
-    src: PropTypes.string.isRequired,
-    imgs: PropTypes.array.isRequired,
-    rooms: PropTypes.number.isRequired,
-    maxCountOfPeople: PropTypes.number.isRequired,
-    price: PropTypes.number.isRequired,
-    stars: PropTypes.number.isRequired,
-    type: PropTypes.string.isRequired,
-    inside: PropTypes.arrayOf(PropTypes.string),
-    host: PropTypes.shape({
-      name: PropTypes.string.isRequired,
-      avatar: PropTypes.string.isRequired,
-      description: PropTypes.string.isRequired,
-    }).isRequired,
-  }).isRequired
+  offerReviews: reviewsShortPropTypes,
+  offer: offerPropTypes,
 };
 
 export default PropertyInformation;
--- src/components/review-form/review-form.jsx
+++ src/components/review-form/review-form.jsx
@@ -1,4 +1,4 @@
-import React, {PureComponent} from "react";
+import React, {PureComponent, Fragment} from "react";
 import PropTypes from "prop-types";
 
 class ReviewForm extends PureComponent {
@@ -10,78 +10,71 @@ class ReviewForm extends PureComponent {
       review: ``
     };
 
-    // this.handleSubmit = this.handleSubmit.bind(this);
+    this.newComment = this.newComment.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.handleFieldChange = this.handleFieldChange.bind(this);
+    this.onFieldsClear = this.onFieldsClear.bind(this);
   }
-  // handleSubmit(onNewReview, evt) {
-  //   evt.preventDefault();
-  //   onNewReview(this.state.rating, this.state.review);
-  // }
+
+  newComment(stars, com) {
+    let review = {
+      comment: com,
+      date: `2019-04-24`,
+      id: 1,
+      rating: Number(stars),
+      user: {
+        avatarUrl: `img/avatar-max.jpg`,
+        id: 4,
+        isPro: false,
+        name: `Max`,
+      }
+    };
+    return (review);
+  }
+
+  handleSubmit(evt) {
+    evt.preventDefault();
+    this.props.onNewReview(this.newComment(this.state.rating, this.state.review));
+    this.onFieldsClear();
+  }
+
   handleFieldChange(evt) {
     const {name, value} = evt.target;
     this.setState({[name]: value});
   }
 
-  // ratingFilds () {
-  //   let content = [];
-  //   for (let i = 0; i < 5; i++) {
-  //     content.push(
-  //       <input onChange={this.handleFieldChange} className="form__rating-input visually-hidden" name="rating" value={i} id="${i}-stars" type="radio" />
-  //       <label key={`label-${i}`} htmlFor="${i}-stars" className="reviews__rating-label form__rating-label" title="good">
-  //         <svg className="form__star-image" width="37" height="33">
-  //           <use xlinkHref="#icon-star"></use>
-  //         </svg>
-  //       </label>
-  //     );
-  //   }
-  //   return content;
-  // }
+  onFieldsClear() {
+    this.setState({
+      review: ``,
+      rating: ``
+    });
+  }
 
   render() {
-    const onNewReview = this.props;
+    const ratingFilds = () => {
+      let title = [`terribly`, `badly`, `not bad`, `good`, `perfect`];
+      let content = [];
+      for (let i = title.length; i >= 1; i--) {
+        content.push(<Fragment key={`stars-${i}`}>
+          <input onChange={this.handleFieldChange} className="form__rating-input visually-hidden" name="rating" value={i} id={`${i}-stars`} type="radio" />
+          <label htmlFor={`${i}-stars`} className="reviews__rating-label form__rating-label" title={title[i - 1]}>
+            <svg className="form__star-image" width="37" height="33">
+              <use xlinkHref="#icon-star"></use>
+            </svg>
+          </label>
+        </Fragment>
+        );
+      }
+      return content;
+    };
+
     return (
-      // <form onSubmit={(evt) => this.handleSubmit(onNewReview, evt)} className="reviews__form form" action="#" method="post">
-      <form onSubmit={(evt) => {
-        evt.preventDefault();
-        onNewReview(this.state.rating, this.state.review);
-      }} className="reviews__form form" action="#" method="post">
+      <form onSubmit={this.handleSubmit} className="reviews__form form" action="#" method="post">
         <label className="reviews__label form__label" htmlFor="review">Your review</label>
         <div className="reviews__rating-form form__rating">
-          <input onChange={this.handleFieldChange} className="form__rating-input visually-hidden" name="rating" value="5" id="5-stars" type="radio" />
-          <label htmlFor="5-stars" className="reviews__rating-label form__rating-label" title="perfect">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-          <input onChange={this.handleFieldChange} className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
-          <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input onChange={this.handleFieldChange} className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
-          <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input onChange={this.handleFieldChange} className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
-          <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
-
-          <input onChange={this.handleFieldChange} className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
-          <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
-            <svg className="form__star-image" width="37" height="33">
-              <use xlinkHref="#icon-star"></use>
-            </svg>
-          </label>
+          {ratingFilds()}
         </div>
-        <textarea onChange={this.handleFieldChange} className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
+        <textarea onChange={this.handleFieldChange} value={this.state.review} className="reviews__textarea form__textarea" id="review" name="review" placeholder="Tell how was your stay, what you like and what can be improved"></textarea>
         <div className="reviews__button-wrapper">
           <p className="reviews__help">
             To submit review please make sure to set <span className="reviews__star">rating</span> and describe your stay with at least <b className="reviews__text-amount">50 characters</b>.
