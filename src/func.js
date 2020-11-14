a7300faf626b2fd193ccf8e4852d98694ce4982f
Natalia <lukashina_natalia@mil.ru>
2020-11-14 17:43:50 +0300

    simplified getArrayOfCities function

--- src/func.js
+++ src/func.js
@@ -1,8 +1,7 @@
 // Возвращает массив неповторяющихся городов
 export const getArrayOfCities = (offers) => {
-  const allParameters = [];
   // Создаём массив из всех значений параметра (с повторами)
-  offers.map((item) => (allParameters.push(item.city.name)));
+  const allParameters = offers.map((item) => item.city.name);
   // Превращаем созданный массив в set, тем самым исключая одинаковые элементы массива
   const uniqueParameters = [...new Set(allParameters)];
   // Сделаем первые буквы заглавными
